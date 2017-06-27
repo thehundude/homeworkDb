822c16d18e8207390c6e968e4c33af9cf3af08fc
thehundude <tamas.reisz@outlook.com>
2017-06-27 20:10:00 +0200

    login megoldása

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import runnersdb.LoginUtil;
+
+import java.sql.SQLException;
+
 /**
  * Created by tamas on 2017. 06. 27..
  */
 public class Main {
     public static void main(String[] args) {
-
+        // login
+        try {
+            LoginUtil.login();
+        } catch (SQLException e) {
+            // e.printStackTrace();
+            System.out.println("Nem létezik ilyen felhasználó!");
+        }
     }
 }
---
+++ src/main/java/runnersdb/LoginUtil.java
@@ -0,0 +1,48 @@
+package runnersdb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+/**
+ * Created by tamas on 2017. 06. 27..
+ */
+public class LoginUtil {
+    private static String usernameInput;
+    private static String passwordInput;
+
+    public static void login () throws SQLException {
+        Connection connection = DbUtil.getConnection();
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Írd be a felhasználóneved: ");
+        usernameInput = reader.nextLine();
+
+        System.out.print("Írd be a jelszavad: ");
+        passwordInput = reader.nextLine();
+
+        PreparedStatement ps = connection.prepareStatement("SELECT * FROM dbuser WHERE user_name='" + usernameInput +
+                "' AND user_password='" + passwordInput + "'");
+
+        ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        User loggedInUser = new User();
+
+        loggedInUser.setUserId(rs.getInt(1));
+        loggedInUser.setUserName((rs.getString(2)));
+        loggedInUser.setUserPassword(rs.getString(3));
+        loggedInUser.setUserCanEdit(rs.getBoolean(4));
+
+        // egyelőre csak feedback, hogy beolvasta a felhasználót, létezik ilyen
+        if (rs != null) {
+            System.out.println("Van ilyen felhasználó.");
+        }
+
+        connection.close();
+    }
+}
